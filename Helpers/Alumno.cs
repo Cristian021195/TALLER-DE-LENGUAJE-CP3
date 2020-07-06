8ef319da050ed548b43865ac3f2ac8ed17d2db02
Cristian021195 <cristiangramajo015@gmail.com>
2020-07-06 17:47:20 -0300

    Se agrego carpeta con las clases Alumno y Helpers

---
+++ Helpers/Alumno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CP3.Helpers
+{
+    public class Alumno
+    {
+        private int id;
+        private string nombre;
+        private int anio_inscripcion;
+        private DateTime nacimiento;
+        private string curso_inscripto;
+        private int edad;
+        private float mensualidad;
+        private bool regular;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Anio_inscripcion { get => anio_inscripcion; set => anio_inscripcion = value; }
+        public DateTime Nacimiento { get => nacimiento; set => nacimiento = value; }
+        public string Curso_inscripto { get => curso_inscripto; set => curso_inscripto = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public float Mensualidad { get => mensualidad; set => mensualidad = value; }
+        public bool Regular { get => regular; set => regular = value; }
+    }
+}
---
+++ Helpers/HelperDeArchivos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.IO;
+
+namespace CP3.Helpers
+{
+    public static class HelperDeArchivos
+    {
+
+        public static void CargarArchivo(string NombreDeArchivo, List<Alumno> alumnos) {
+            string ruta = NombreDeArchivo;
+            string txtSTR = "";
+            for (int i = 0; i < alumnos.Count; i++) {
+            txtSTR = alumnos[i].Id.ToString() + ";" + alumnos[i].Nombre + ";" + alumnos[i].Anio_inscripcion.ToString() + ";" + alumnos[i].Nacimiento.ToString("yyyy-MM-dd") + ";" + alumnos[i].Curso_inscripto + ";" + alumnos[i].Edad.ToString() + ";" + alumnos[i].Mensualidad.ToString() + ";" + alumnos[i].Regular.ToString();
+                if (alumnos[i].Curso_inscripto == "ArtesPlasticas") {
+                    using (Stream fs = new FileStream(ruta+ "ArtesPlasticas.csv", FileMode.Append, FileAccess.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(fs)) {
+                            sw.WriteLine(txtSTR);
+                        }
+                    }
+
+                    }else if (alumnos[i].Curso_inscripto == "ArteDigital") {
+                    using (Stream fs2 = new FileStream(ruta+ "ArteDigital.csv", FileMode.Append, FileAccess.Write))
+                    {
+                        using (StreamWriter sw2 = new StreamWriter(fs2))
+                        {
+                            sw2.WriteLine(txtSTR);
+                        }
+                    }
+                }
+                else if (alumnos[i].Curso_inscripto == "DisenioGrafico") {
+                    
+                    using (Stream fs3 = new FileStream(ruta+ "DisenioGrafico.csv", FileMode.Append, FileAccess.Write))
+                    {
+                        using (StreamWriter sw3 = new StreamWriter(fs3))
+                        {
+                            sw3.WriteLine(txtSTR);
+                        }
+                    }                    
+
+                }
+            }
+        }
+    }
+}
